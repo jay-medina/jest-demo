d42e4c07ccd5c0d884c22f98d0cc4da345845ea5
Jose Medina <josemedina921@gmail.com>
2017-12-29 09:11:48 -0800

    adding broken test

--- src/testFile.test.ts
+++ src/testFile.test.ts
@@ -1,4 +1,4 @@
-jest.mock('./helper', () => ({ logger: jest.fn()}));
+jest.mock('./helper', () => ({ logger: jest.fn() }));
 
 import { testFunction } from './testFile';
 import { logger } from './helper';
@@ -6,11 +6,11 @@ import { logger } from './helper';
 describe('testFile', () => {
   describe('when invoked', () => {
     beforeEach(() => {
-      
+      testFunction('errorMsg');
     });
 
     it('logs the message', () => {
-
+      expect(logger).toHaveBeenCalledWith('errorMsg');
     });
   });
 });
