a0a5941aa5ae348b5e99aea21e37359700edf883
Jose Medina <josemedina921@gmail.com>
2017-12-29 09:38:10 -0800

    adding additional tests

--- src/javascript_version/testFile.test.js
+++ src/javascript_version/testFile.test.js
@@ -9,8 +9,28 @@ describe('testFile', () => {
       testFunction('errorMsg');
     });
 
-    it('logs the message', () => {
+    it('fails to print the error line', () => {
       expect(logger).toHaveBeenCalledWith('errorMsg');
+    });
+  });
+
+  describe('when invoked', () => {
+    beforeEach(() => {
+      testFunction('errorMsg');
+    });
+
+    it('prints the error line now', () => {
+      expect(logger).toHaveBeenCalledWith('errorMsg', 'fail on purpose');
+    });
+  });
+
+  describe('when invoked', () => {
+    beforeEach(() => {
+      testFunction('errorMsg');
+    });
+
+    it('passes only when explicit', () => {
+      expect(logger).toHaveBeenCalledWith('errorMsg', undefined);
     });
   });
 });
